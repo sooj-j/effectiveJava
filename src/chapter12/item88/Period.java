3a9a1a46c3138a75e7018436ec79673f3304b5ba
sooj-j <jsujin9603@gmail.com>
2020-10-04 22:59:20 +0900

    item88 - 방어적 복사

--- src/chapter12/item88/Period.java
+++ src/chapter12/item88/Period.java
@@ -7,8 +7,8 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class Period implements Serializable {
-	private final Date start;
-	private final Date end;
+	private Date start;
+	private Date end;
 
 	public Period(Date start, Date end) {
 		this.start = new Date(start.getTime());
@@ -27,6 +27,10 @@ public class Period implements Serializable {
 
 	private void readObject(ObjectInputStream s) throws IOException, ClassNotFoundException {
 		s.defaultReadObject();
+
+		// 가변 요소들을 방어적으로 복사한다
+		start = new Date(start.getTime());
+		end = new Date(end.getTime());
 
 		// 불변식을 만족하는지 검사한다.
 		if (start.compareTo(end) > 0)
